f39a5f2494905f1ec3763615d2a4e6d08c628e92
Tester <tester@test.com>
2024-02-16 16:55:52 +0200

    Add methods to work with Spending Table.

--- niffler-hw-tests/src/test/java/guru/qa/niffler/pages/component/SpendingTable.java
+++ niffler-hw-tests/src/test/java/guru/qa/niffler/pages/component/SpendingTable.java
@@ -1,11 +1,17 @@
 package guru.qa.niffler.pages.component;
 
+import com.codeborne.selenide.SelenideElement;
 import guru.qa.niffler.model.SpendJson;
+import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.condition.SpendCollectionCondition.spends;
 
 public class SpendingTable extends BaseComponent<SpendingTable> {
+
+    private SelenideElement descriptionInput = $(".editable__input[name='description']");
+    private SelenideElement saveRowBtn = $(".spendings__button-group .button-icon_type_submit");
 
     public SpendingTable() {
         super($(".spendings-table tbody"));
@@ -15,4 +21,36 @@ public class SpendingTable extends BaseComponent<SpendingTable> {
         getSelf().$$("tr").should(spends(expectedSpends));
         return this;
     }
+
+    @Step("Select spend by index '{index}'")
+    public void selectSpendByIndex(int index) {
+        getSelf().$$("tr")
+                .get(index)
+                .$("td")
+                .scrollTo()
+                .click();
+    }
+
+    @Step("Select spend by description '{description}'")
+    public void selectSpendByDescription(String description) {
+        getSelf().$$("tr")
+                .find(text(description))
+                .$("td")
+                .scrollTo()
+                .click();
+    }
+
+    @Step("Click update spend btn by description '{description}'")
+    public void clickUpdateSpendBtnByDescription(String oldDesc, String newDesc) {
+        getSelf().$$("tr")
+                .find(text(oldDesc))
+                .$(".button-icon_type_edit")
+                .scrollTo()
+                .click();
+
+        descriptionInput.setValue(newDesc);
+        saveRowBtn.scrollTo();
+        saveRowBtn.click();
+    }
+
 }
