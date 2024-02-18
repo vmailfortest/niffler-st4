20193f3a505a0f7031ac0a59a934851e39ee4c0a
Tester <tester@test.com>
2024-02-18 23:25:56 +0200

    Fix SpendCollectionCondition.

--- niffler-hw-tests/src/test/java/guru/qa/niffler/condition/SpendCollectionCondition.java
+++ niffler-hw-tests/src/test/java/guru/qa/niffler/condition/SpendCollectionCondition.java
@@ -35,64 +35,76 @@ public class SpendCollectionCondition {
 
                 if (elements.size() != expectedSpends.length) {
 
-                    failedValues.append("Actual rows").append("\n");
+                    failedValues.append("Incorrect table size.").append("\n");
+                    failedValues.append("   Actual rows:").append("\n");
                     for (WebElement element : elements) {
                         failedValues.append(element.getText().replace("\n", " / ")).append("\n");
+                    }
+
+                    failedValues.append("   Expected rows:").append("\n");
+                    for (SpendJson spend : expectedSpends) {
+                        failedValues
+                                .append(formatter.format(spend.spendDate())).append(" / ")
+                                .append(String.format("%.0f", spend.amount())).append(" / ")
+                                .append(spend.currency().toString()).append(" / ")
+                                .append(spend.category()).append(" / ")
+                                .append(spend.description())
+                                .append("\n");
                     }
 
                     return CheckResult.rejected("Incorrect table size.", failedValues);
                 }
 
-                for (WebElement element : elements) {
+                boolean isPassed = true;
 
-                    failedValues.append("Actual / Expected").append("\n");
+                failedValues.append("Actual / Expected").append("\n");
 
-                    List<WebElement> tds = element.findElements(By.cssSelector("td"));
+                for (int i = 0; i < elements.size(); i++) {
 
-                    boolean isPassed = true;
+                    SpendJson expectedSpend = expectedSpends[i];
 
-                    for (SpendJson expectedSpend : expectedSpends) {
+                    failedValues.append("   Row " + i).append("\n");
 
-                        String text = tds.get(1).getText();
-                        if (!text.equals(formatter.format(expectedSpend.spendDate()))) {
-                            isPassed = false;
-                            failedValues.append(text).append(" / ").append(expectedSpend.spendDate()).append("\n");
-                        }
+                    List<WebElement> tds = elements.get(i).findElements(By.cssSelector("td"));
 
-                        text = tds.get(2).getText();
-                        if (!text.equals(String.format("%.0f", expectedSpend.amount()))) {
-                            isPassed = false;
-                            failedValues.append(text).append(" / ").append(expectedSpend.amount()).append("\n");
-                        }
-
-                        text = tds.get(3).getText();
-                        if (!text.equals(expectedSpend.currency().toString())) {
-                            isPassed = false;
-                            failedValues.append(text).append(" / ").append(expectedSpend.currency()).append("\n");
-                        }
-
-                        text = tds.get(4).getText();
-                        if (!text.equals(expectedSpend.category())) {
-                            isPassed = false;
-                            failedValues.append(text).append(" / ").append(expectedSpend.category()).append("\n");
-                        }
-
-                        text = tds.get(5).getText();
-                        if (!text.equals(expectedSpend.description())) {
-                            isPassed = false;
-                            failedValues.append(text).append(" / ").append(expectedSpend.description()).append("\n");
-                        }
-
+                    String text = tds.get(1).getText();
+                    if (!text.equals(formatter.format(expectedSpend.spendDate()))) {
+                        isPassed = false;
+                        failedValues.append(text).append(" / ").append(expectedSpend.spendDate()).append("\n");
                     }
 
-                    if (isPassed) {
-                        return CheckResult.accepted();
-                    } else {
-                        return CheckResult.rejected("Incorrect spends content.", failedValues.toString());
+                    text = tds.get(2).getText();
+                    if (!text.equals(String.format("%.0f", expectedSpend.amount()))) {
+                        isPassed = false;
+                        failedValues.append(text).append(" / ").append(expectedSpend.amount()).append("\n");
                     }
+
+                    text = tds.get(3).getText();
+                    if (!text.equals(expectedSpend.currency().toString())) {
+                        isPassed = false;
+                        failedValues.append(text).append(" / ").append(expectedSpend.currency()).append("\n");
+                    }
+
+                    text = tds.get(4).getText();
+                    if (!text.equals(expectedSpend.category())) {
+                        isPassed = false;
+                        failedValues.append(text).append(" / ").append(expectedSpend.category()).append("\n");
+                    }
+
+                    text = tds.get(5).getText();
+                    if (!text.equals(expectedSpend.description())) {
+                        isPassed = false;
+                        failedValues.append(text).append(" / ").append(expectedSpend.description()).append("\n");
+                    }
+
                 }
 
-                return super.check(driver, elements);
+                if (isPassed) {
+                    return CheckResult.accepted();
+                } else {
+                    return CheckResult.rejected("Incorrect spends content.", failedValues.toString());
+                }
+
             }
 
             @Override
--- niffler-hw-tests/src/test/java/guru/qa/niffler/test/Lesson12Test.java
+++ niffler-hw-tests/src/test/java/guru/qa/niffler/test/Lesson12Test.java
@@ -1,11 +1,16 @@
 package guru.qa.niffler.test;
 
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.api.SpendApiClient;
 import guru.qa.niffler.jupiter.annotation.GenerateSpend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.pages.message.SuccessMsg;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,18 +23,46 @@ public class Lesson12Test extends BaseWebTest {
         loginPage.login("duck", "12345");
     }
 
-    @GenerateSpend(
-            username = "duck",
-            description = "",
-            amount = 72500.00,
-            category = "Обучение",
-            currency = CurrencyValues.RUB
-    )
+//    @GenerateSpend(
+//            username = "duck",
+//            description = "",
+//            amount = 72500.00,
+//            category = "Обучение",
+//            currency = CurrencyValues.RUB
+//    )
     @Test
-    void spendTableValuesAreCorrect(SpendJson spend) {
+    void spendTableValuesAreCorrect() throws IOException {
+
+        SpendJson spend1 = new SpendJson(
+                null, new Date(), "apiCategory", CurrencyValues.USD,10d, "spend_1", "duck"
+        );
+        SpendJson spend2 = new SpendJson(
+                null, new Date(), "apiCategory", CurrencyValues.EUR,20d, "spend_2", "duck"
+        );
+        SpendJson spend3 = new SpendJson(
+                null, new Date(), "apiCategory", CurrencyValues.RUB,30d, "spend_3", "duck"
+        );
+        SpendJson spend4 = new SpendJson(
+                null, new Date(), "apiCategory", CurrencyValues.KZT,40d, "spend_4", "duck"
+        );
+
+        SpendApiClient spendApiClient = new SpendApiClient();
+        spendApiClient.addSpend(spend1);
+        spendApiClient.addSpend(spend2);
+        spendApiClient.addSpend(spend3);
+        spendApiClient.addSpend(spend4);
+
+//        spend2 = new SpendJson(
+//                null, new Date(), "apiCategory", CurrencyValues.EUR,25d, "spend_2_upd2", "duck"
+//        );
+//        spend4 = new SpendJson(
+//                null, new Date(), "apiCategory", CurrencyValues.KZT,45d, "spend_4_upd4", "duck"
+//        );
+
+        Selenide.refresh();
 
         mainPage.spendingsTableShouldHaveRows();
-        mainPage.getSpendingTable().checkSpends(spend);
+        mainPage.getSpendingTable().checkSpends(spend1, spend2, spend3, spend4);
 
     }
 
