896052e7bf7320b928aa17a4de2ab08acbd76d1d
Tester <tester@test.com>
2024-03-07 08:50:16 +0200

    Add userdata gRPC service.

--- niffler-hw-tests/src/test/java/guru/qa/niffler/config/DockerConfig.java
+++ niffler-hw-tests/src/test/java/guru/qa/niffler/config/DockerConfig.java
@@ -33,4 +33,9 @@ public class DockerConfig implements Config {
     public String currencyGrpcHost() {
         return "currency.niffler.dc";
     }
+
+    @Override
+    public String userdataGrpcHost() {
+        return "userdata.niffler.dc";
+    }
 }
